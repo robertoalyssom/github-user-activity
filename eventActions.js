bc54e35381fce2cbb7510c091ba130509a9f4c33
robertoalyssom <robert_alys@hotmail.com>
2026-05-28 12:05:01 -0300

    Refact: event message handling by moving eventActions to a separate module

---
+++ eventActions.js
@@ -0,0 +1,43 @@
+const onRepo = (text, { name }) => `${text} on ${name}`;
+const withCount = (verb, noun) => (event) =>
+  onRepo(`${verb} ${event.count} ${noun}`, event);
+
+export const eventActions = {
+  CommitCommentEvent: withCount("Created", "commit comment"),
+  CreateEvent: withCount("Created, branch or tag"),
+  DeleteEvent: withCount("Deleted", "branch or tag"),
+  DiscussionCommentEvent: withCount("Commented on", "discussion"),
+  DiscussionEvent: withCount("Created or updated", "discussion"),
+  DeploymentEvent: withCount("Created", "deployment"),
+  DeploymentStatusEvent: withCount("Updated", "deployment status"),
+  DownloadEvent: withCount("Created", "download"),
+  FollowEvent: withCount("Followed", "user"),
+  ForkEvent: withCount("Forked", "repository"),
+  GollumEvent: withCount("Updated", "wiki page"),
+  IssueCommentEvent: withCount("Commented on", "issue"),
+  IssuesEvent: withCount("Opened, closed, or updated", "issue"),
+  MemberEvent: withCount("Added", "collaborator"),
+  PageBuildEvent: withCount("Triggered", "GitHub Pages build"),
+  ProjectCardEvent: withCount("Updated", "project card"),
+  ProjectColumnEvent: withCount("Updated", "project column"),
+  ProjectEvent: withCount("Updated", "project"),
+  PublicEvent: withCount("Made", "repository public"),
+  PullRequestEvent: withCount("Opened, closed, or updated", "pull request"),
+  PullRequestReviewEvent: withCount("Reviewed", "pull request"),
+  PullRequestReviewCommentEvent: withCount(
+    "Commented on",
+    "pull request review",
+  ),
+  PullRequestReviewThreadEvent: withCount(
+    "Updated",
+    "pull request review thread",
+  ),
+  PushEvent: ({ name, count }) => `Pushed ${count} commits to ${name}`,
+  RepositoryEvent: withCount("Created or updated", "repository"),
+  RepositoryImportEvent: withCount("Imported", "repository"),
+  ReleaseEvent: withCount("Published", "release"),
+  StatusEvent: withCount("Updated", "commit status"),
+  SponsorshipEvent: withCount("Created or updated", "sponsorship"),
+  TeamAddEvent: withCount("Added", "user or repository to a team"),
+  WatchEvent: withCount("Starred", "repository"),
+};
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 import { error } from "console";
 import process from "process";
+import { eventActions } from "./eventActions.js";
 
 const [, , ...args] = process.argv;
 const [username] = args;
@@ -15,7 +16,7 @@ const githubEvents = await fetchGithub();
 const repoEvents = filterRepo(githubEvents);
 const eventsMessages = setEventMessage(repoEvents);
 
-eventsMessages.forEach((message) => console.log("- ", message));
+eventsMessages.forEach((message) => console.log("-", message));
 
 async function fetchGithub() {
   try {
@@ -61,50 +62,8 @@ function countType(events, accRepo) {
 }
 
 function setEventMessage(repoEvents) {
-  const onRepo = (text, { name }) => `${text} on ${name}`;
-  const withCount = (verb, noun) => (event) =>
-    onRepo(`${verb} ${event.count} ${noun}`, event);
-
-  const actions = {
-    CommitCommentEvent: withCount("Created", "commit comment"),
-    CreateEvent: withCount("Created, branch or tag"),
-    DeleteEvent: withCount("Deleted", "branch or tag"),
-    DiscussionCommentEvent: withCount("Commented on", "discussion"),
-    DiscussionEvent: withCount("Created or updated", "discussion"),
-    DeploymentEvent: withCount("Created", "deployment"),
-    DeploymentStatusEvent: withCount("Updated", "deployment status"),
-    DownloadEvent: withCount("Created", "download"),
-    FollowEvent: withCount("Followed", "user"),
-    ForkEvent: withCount("Forked", "repository"),
-    GollumEvent: withCount("Updated", "wiki page"),
-    IssueCommentEvent: withCount("Commented on", "issue"),
-    IssuesEvent: withCount("Opened, closed, or updated", "issue"),
-    MemberEvent: withCount("Added", "collaborator"),
-    PageBuildEvent: withCount("Triggered", "GitHub Pages build"),
-    ProjectCardEvent: withCount("Updated", "project card"),
-    ProjectColumnEvent: withCount("Updated", "project column"),
-    ProjectEvent: withCount("Updated", "project"),
-    PublicEvent: withCount("Made", "repository public"),
-    PullRequestEvent: withCount("Opened, closed, or updated", "pull request"),
-    PullRequestReviewEvent: withCount("Reviewed", "pull request"),
-    PullRequestReviewCommentEvent: withCount(
-      "Commented on",
-      "pull request review",
-    ),
-    PullRequestReviewThreadEvent: withCount(
-      "Updated",
-      "pull request review thread",
-    ),
-    PushEvent: ({ name, count }) => `Pushed ${count} commits to ${name}`,
-    RepositoryEvent: withCount("Created or updated", "repository"),
-    RepositoryImportEvent: withCount("Imported", "repository"),
-    ReleaseEvent: withCount("Published", "release"),
-    StatusEvent: withCount("Updated", "commit status"),
-    SponsorshipEvent: withCount("Created or updated", "sponsorship"),
-    TeamAddEvent: withCount("Added", "user or repository to a team"),
-    WatchEvent: withCount("Starred", "repository"),
-  };
-
-  const eventMessages = repoEvents.map((event) => actions[event.type]?.(event)); // If the event type is not in the actions object, undefined is returned
+  const eventMessages = repoEvents.map((event) =>
+    eventActions[event.type]?.(event),
+  ); // If the event type is not in the eventActions object, undefined is returned
   return eventMessages.filter(Boolean); // Filter out any falsy values (undefined)
 }
